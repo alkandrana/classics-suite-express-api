e529f1df0c44bc5555248fd942b8bf626a8dd58c
Rosa Myers <larosaardente@gmail.com>
2026-05-10 13:50:16 -0700

    Used work sections to create pagination for the work display page.

--- api/src/controllers/opus.controller.js
+++ api/src/controllers/opus.controller.js
@@ -1,20 +1,34 @@
 import 'dotenv/config';
 import {drizzle} from 'drizzle-orm/libsql';
-import {eq} from 'drizzle-orm';
-import {opera} from '../db/schema.js';
+import {count, eq} from 'drizzle-orm';
+import {opera, lines, authors, languages} from '../db/schema.js';
 
 const db = drizzle(process.env.DB_FILE_NAME);
 
 export async function getOpera(req, res) {
     const opusList = await db.select().from(opera);
+    for (const op of opusList) {
+        let [lineCount] = await db.select({count: count()}).from(lines).where(eq(lines.opusId, op.id));
+        op.lineCount = lineCount.count;
+    }
     return res.json(opusList);
 }
 
 export async function getOpus(req, res) {
     const opusId = req.params.id;
     const [opus] = await db.select().from(opera).where(eq(opera.id, opusId));
+    [opus.author] = await db.select().from(authors).where(eq(authors.id, opus.authorId));
+    [opus.language] = await db.select().from(languages).where(eq(languages.id, opus.languageId));
+    const opusLines = await db.select().from(lines).where(eq(lines.opusId, opusId));
+    const sectionedLines = Object.groupBy(opusLines, ({locus}) => {
+        let secs = locus.split(".");
+        let currentSec = secs.slice(0, secs.length - 1);
+        return currentSec.length == 0 ? "unsectioned" : currentSec[0];
+    });
+    opus.lines = sectionedLines;
     return res.json(opus);
 }
+
 
 export async function createOpus(req, res) {
     const opus = req.body;
